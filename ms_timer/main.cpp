0a64bc18c783531819a02ddf76956caaaf14bcce
zhengbo <zb@qq.com>
2020-04-15 21:26:21 +0800

    add logo

--- ms_timer/main.cpp
+++ ms_timer/main.cpp
@@ -1,9 +1,11 @@
 #include "ms_timer.h"
 #include <QtWidgets/QApplication>
 
+
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+	QApplication::setWindowIcon(QIcon(":/res/logo.ico"));
 	MsTimer w;
 	w.show();
 	return a.exec();
